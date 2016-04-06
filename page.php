59a5e10752e6716d7334b3a2b5bc76b995231333
Justin Fester <justinlfester@gmail.com>
2016-04-06 15:49:36 +0200

    cutom pemplate pages
    customised the woo commerce pages to be more in line with the zandism
    look and feel

--- page.php
+++ page.php
@@ -1,23 +1,33 @@
-<?php get_header(); ?>
-
-
-
-<section id="about">
-
- <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
- 
-      <h1><?php the_title(); ?></h1>
-     
-      <p><?php the_content();?></p>
-      
-      <?php endwhile; else : ?>
-	    <p><?php _e( 'Sorry, no pages found' ); ?></p>
-     <?php endif; ?>
-
-    </section>
-    
-
-
-
-
+<?php
+/*
+  Template Name: page
+  
+  */
+?>
+
+
+<?php get_header(); ?>
+
+         
+         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+              <h1><?php the_title(); ?></h1>
+            
+          
+              <p><?php the_content(); ?></p>
+          
+         
+          
+        <?php endwhile; else : ?>
+	     <p><?php _e( 'Sorry, no pages found' ); ?></p>
+        <?php endif; ?>
+          
+      
+  
+  
+      
+      
+
+    
+
 <?php get_footer(); ?>
